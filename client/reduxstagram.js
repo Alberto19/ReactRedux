c0c8965f497dee43bfea132870f45ae12177f0fc
Junior <jose.junior@sequenza.com.br>
2016-12-06 13:36:46 -0200

    Criando componentes Aula 9

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -6,5 +6,23 @@ import css from './styles/style.styl';
 
 //import components
 import Main from './components/Main';
+import Single from './components/Single';
+import PhotoGrid from './components/PhotoGrid';
 
-render(<Main/>, document.getElementById('root'));
+// import react routes deps
+import {Router, Route, IndexRoute, browserHistory} from 'react-router';
+import {Provider} from 'react-redux';
+import  store, {history}  from './store'
+
+const router = (
+    <Provider store={store}>
+        <Router history={history}>
+            <Route path="/" component={Main}>
+                <IndexRoute component={PhotoGrid}></IndexRoute>
+                <Route path="/view/:postId" component={Single}></Route>
+            </Route>
+        </Router>
+    </Provider>
+)
+
+render(router, document.getElementById('root'));
